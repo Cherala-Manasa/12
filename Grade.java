4986bea36e40ec2bf19160a5734ff4ce75d981d4
Cherala-Manasa <manasacherala679@gmail.com>
2024-04-02 23:16:22 +0530

    1

---
+++ Grade.java
@@ -0,0 +1,16 @@
+public class Grade {
+    public static void main(String[] args){
+        int a=80;
+        if(a>90 & a<=100)
+        System.out.println("grade A");
+        else if(a>80 & a<=90)
+        System.out.println("grade B");
+        else if(a>70 & a<=80)
+        System.out.println("grade C");
+        else if(a>60 & a<=70)
+        System.out.println("grade D");
+        else
+        System.out.println("FAIL!!!");
+    
+    }
+}
